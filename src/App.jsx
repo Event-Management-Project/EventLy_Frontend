dd0de857c02a30ed56f699814215ec96702003d7
Sourabh <magdumsourabhh@gmail.com>
2025-06-20 15:01:00 +0530

    customer module components with basic JSX structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import CustomerProfile from "./components/CustomerProfile";
+import CustomerSidebar from "./components/CustomerSidebar";
+import TicketSuccessPage from "./components/TicketSuccessPage";
+import CustomerNotifications from "./components/CustomerNotifications";
+import CustomerNavbar from "./components/CustomerNavbar";
+import BookingHistory from "./components/BookingHistory";
 
 function App() {
   return (
-<h1 className="bg-red-500">
-      App
-    </h1>
-  )
+    <div>
+      <CustomerProfile />
+      <CustomerSidebar />
+      <CustomerNavbar/>
+      <CustomerNotifications />
+      <BookingHistory/>
+      <TicketSuccessPage />
+    </div>
+  );
 }
 
-export default App
+export default App;
