76d5977503051ce35e9ef80c500bb9ae48cc0b78
Sourabh <magdumsourabhh@gmail.com>
2025-06-22 13:35:00 +0530

    changed the booking history and the customer sidebar code with the static code

--- src/components/CustomerSidebar.jsx
+++ src/components/CustomerSidebar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 function CustomerSidebar() {
   return (
-    <div>CustomerSidebar</div>
-  )
+    <div>
+      <div>Home</div>
+      <div>Events</div>
+      <div>Booking History</div>
+      <div>About Us</div>
+      <div>Contact Us</div>
+
+      <hr className="my-4" />
+
+      <div>Profile</div>
+      <div>Logout</div>
+    </div>
+  );
 }
 
-export default CustomerSidebar
+export default CustomerSidebar;
